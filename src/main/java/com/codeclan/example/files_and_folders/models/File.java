82a65a3d1bc1044316e8de378cb13251abc7df23
d7o1d1s0 <d7sarmy@gmail.com>
2021-10-12 18:34:02 +0100

    file class done

--- src/main/java/com/codeclan/example/files_and_folders/models/File.java
+++ src/main/java/com/codeclan/example/files_and_folders/models/File.java
@@ -1,4 +1,67 @@
 package com.codeclan.example.files_and_folders.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "files")
+
 public class File {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="extension")
+    private String extension;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name="folder_id")
+    private Folder folder;
+
+    public File(String name, String extension, Folder folder) {
+        this.name = name;
+        this.extension = extension;
+        this.folder = folder;
+    }
+
+    public File() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
 }
